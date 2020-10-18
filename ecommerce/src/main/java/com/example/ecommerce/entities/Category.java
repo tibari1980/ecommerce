d40ee148e8244f504ee741707334eae05f159d3b
tibari1980 <tibarinewdzign@gmail.com>
2020-10-18 21:49:00 +0200

    Third commit create two entities product and category

--- ecommerce/src/main/java/com/example/ecommerce/entities/Category.java
+++ ecommerce/src/main/java/com/example/ecommerce/entities/Category.java
@@ -1,5 +1,66 @@
 package com.example.ecommerce.entities;
 
-public class Category {
+import java.io.Serializable;
+import java.util.Collection;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="CATEGORIES")
+public class Category implements Serializable {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="CODE")
+	private Long code;
+	@Column(name="NAME")
+	private String name;
+	@Column(name="DESCRIPTION")
+	private String description;
+	@OneToMany(mappedBy = "category")
+	private Collection<Product> products;
+	public Category() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Category(String name, String description, Collection<Product> products) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.products = products;
+	}
+	public Long getCode() {
+		return code;
+	}
+	public void setCode(Long code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Collection<Product> getProducts() {
+		return products;
+	}
+	public void setProducts(Collection<Product> products) {
+		this.products = products;
+	}
+	@Override
+	public String toString() {
+		return "Category [code=" + code + ", name=" + name + ", description=" + description + "]";
+	}
+	
 }
