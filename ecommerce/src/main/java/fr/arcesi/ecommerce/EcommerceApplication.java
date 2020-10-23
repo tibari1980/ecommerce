8c1cda79ebb0f2804fafd157e5e409fd03fc8b96
tibari1980 <tibarinewdzign@gmail.com>
2020-10-23 17:25:12 +0200

    Modification de fichier propertieses

--- ecommerce/src/main/java/fr/arcesi/ecommerce/EcommerceApplication.java
+++ ecommerce/src/main/java/fr/arcesi/ecommerce/EcommerceApplication.java
@@ -1,5 +1,7 @@
 package fr.arcesi.ecommerce;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,22 +32,33 @@ public class EcommerceApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		repositoryRestConfiguration.exposeIdsFor(Product.class);
 		repositoryRestConfiguration.exposeIdsFor(Category.class);
-		for(int i=0;i<10;i++) {
-			Category cat=null;
+		List<Category> lstCategories=Arrays.asList(new Category("Créme visage","créme visage ",null),
+				                                   new Category("Anti-âge", "anti-age", null),
+				                                    new Category("Serum et huile", "Serum et huile",null),
+				                                    new Category("Soin yeux","soin yeux",null),
+				                                    new Category("Nettoyant et démaquillant","nettoyant et démaquillant",null),
+				                                    new Category("Masque","masque",null),
+				                                    new Category("Hydratation","hydratation",null),
+				                                    new Category("Purifiant","purifiant",null),
+				                                    new Category("Créme main","créme main",null),
+	    	                                    new Category("Savons","savons description",null));
+		lstCategories.stream().forEach(cat->{
 			Random r=new Random();
 			int lowCat=1;
 			int hightCat=10;
 			int resultaCat=r.nextInt(hightCat-lowCat)+lowCat;
-			cat=categoryRepository.save(new Category("Categorie"+i, "DescriptionCategorie"+i, "unknown.jpeg"));
+			cat=categoryRepository.save(cat);
 			double lowPrice=20;
 			double hightPrice=1100;
 			for(int a=0;a<20;a++) {
 				Random rp=new Random();
 				double resultPrice=rp.nextDouble()+hightPrice;
-			   productRepository.save(new Product("name"+i, "description"+i, resultPrice , r.nextBoolean(), r.nextBoolean(), r.nextBoolean(), "unknown.jpg", cat));
+			   productRepository.save(new Product("name"+cat.getCode(), "description"+cat.getCode(), resultPrice , r.nextBoolean(), r.nextBoolean(), r.nextBoolean(), "unknown.jpg", cat));
 		   }
-		}
 		
+
+		});
+					
 	
 	}
 
